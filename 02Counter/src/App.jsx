4c058732b3fcfb567a98024b80a922f39f649829
Pradhan321 <avinashpradhan717@gmail.com>
2025-08-15 23:12:58 +0530

    understand the actual meaning of hooks which is useState and how react behave with ui components

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(10)
+
+  const addMe=()=>{
+    if(count >=20){
+      alert("Maximum limit reached");
+      return;
+    }
+    setCount(count+1)
+  }
+
+  const lessMe=()=>{
+    if(count <=0){
+      alert("Minimum limit reached");
+      return;
+    }
+    setCount(count-1)
+  }
+
+  return (
+    <>
+      <h1>Counter game intiated by Avinash Pradhan</h1>
+      <button onClick={addMe}>ADD ME : {count}</button>
+      <br />
+      <br />
+      <button onClick={lessMe}>LESS ME : {count}</button>
+    </>
+  )
+}
+
+export default App
